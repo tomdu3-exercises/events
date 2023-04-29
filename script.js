ef11efd786d90a253893af52c9429634230fdab5
Tomislav Dukez <tomdu3@ymail.com>
2023-04-29 21:53:34 +0200

    lesson2: effects

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 $(document).ready(function () {
   $("#stream1_btn").on("click", function () {
-    $(".stream1").removeClass("highlight_stream");
-    $(".stream2").removeClass("highlight_stream");
-    $(".stream3").removeClass("highlight_stream");
-    $(".stream1").addClass("highlight_stream");
+    // .show() method and .hide() method
+    //toggle() method
+    $(".stream1").slideUp();
+    $(".stream1").show("slow");
+    $(".stream1").show("medium");
+    $(".stream1").show("fast");
+    $(".stream1").show(1000);
   });
   $("#stream2_btn").on("click", function () {
-    $(".stream1").removeClass("highlight_stream");
+    $(".stream1").slideToggle("slow");
     $(".stream2").removeClass("highlight_stream");
     $(".stream3").removeClass("highlight_stream");
     $(".stream2").addClass("highlight_stream");
   });
   $("#stream3_btn").on("click", function () {
     $(".stream1").removeClass("highlight_stream");
-    $(".stream2").removeClass("highlight_stream");
+    $(".stream2").fadeTo(1000, 0.5);
     $(".stream3").removeClass("highlight_stream");
     $(".stream3").addClass("highlight_stream");
   });
